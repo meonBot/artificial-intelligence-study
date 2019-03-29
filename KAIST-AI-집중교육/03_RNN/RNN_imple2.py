da5a392b82dccfb03ed2f674c5dc35d570f2d182
jukyellow <45334819+jukyellow@users.noreply.github.com>
2019-03-30 01:59:19 +0900

    RNN 교육내용정리, 실습파일추가

---
+++ KAIST-AI-집중교육/03_RNN/RNN_imple2.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import os
+import shutil
+import tensorflow as tf
+import models2
+import matplotlib.pylab as plt
+
+### load data and split it to training and test data###
+data = np.loadtxt('stock_data.txt')
+data_train_X = data[:450,:]
+data_train_Y = data[1:451,:]
+data_test_X  = data[450:-1,:]
+data_test_Y  = data[451:,:]
+
+### Hyperparameters ### 
+INPUT_SIZE    = 9
+OUTPUT_SIZE   = 9
+HIDDEN_SIZE   = 100
+LEARNING_RATE = 0.005
+nEpoch 	      = 500
+
+### Grapch ### 
+# placehoders for inputs and outputs
+inputs   = tf.placeholder(tf.float32, (None, INPUT_SIZE))  
+outputs  = tf.placeholder(tf.float32, (None, OUTPUT_SIZE)) 
+inputs_  = tf.expand_dims(inputs,0)
+outputs_ = tf.expand_dims(outputs,0)
+
+# RNN/LSTM layer
+#cell = tf.nn.rnn_cell.BasicRNNCell(HIDDEN_SIZE)
+#cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_SIZE)
+#cell = tf.nn.rnn_cell.GRUCell(HIDDEN_SIZE)
+#cell = models2.BasicRNNCell(HIDDEN_SIZE)
+cell = models2.BasicLSTMCell(HIDDEN_SIZE)
+#cell = models2.GRUCell(HIDDEN_SIZE)
+rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, inputs_, dtype=tf.float32)
+
+# Output layer 
+pred = tf.layers.dense(rnn_outputs, OUTPUT_SIZE)
+
+# objective function 
+mse = tf.reduce_mean(tf.square(pred - outputs_))
+
+tf.summary.scalar("loss", mse)
+merge_op = tf.summary.merge_all()
+
+#optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
+minimize = optimizer.minimize(mse)
+
+# graph information
+#size = lambda v: reduce(lambda x, y: x*y, v.get_shape().as_list())
+#n = sum(size(v) for v in tf.trainable_variables())
+#print("Model size: %d" % n)
+
+### run ### 
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+if os.path.exists('train'):
+  shutil.rmtree('train')
+if os.path.exists('val'):
+  shutil.rmtree('val')
+os.mkdir('train')
+os.mkdir('val')
+train_writer = tf.summary.FileWriter('train', sess.graph)
+val_writer = tf.summary.FileWriter('val', sess.graph)
+
+for i in range(nEpoch):
+  _, train_mse, summary = sess.run([minimize, mse, merge_op], {inputs: data_train_X, outputs: data_train_Y})
+  train_writer.add_summary(summary, i)
+  test_mse, summary = sess.run([mse, merge_op], {inputs: data_test_X, outputs: data_test_Y})
+  val_writer.add_summary(summary, i)
+  print("Epoch: %d Train MSE: %.5f Test MSE: %.5f " % (i+1, train_mse, test_mse))
+
+pred_train = sess.run(pred, {inputs:data_train_X, outputs:data_train_Y})
+pred_test = sess.run(pred, {inputs:data_test_X, outputs:data_test_Y})
---
+++ KAIST-AI-집중교육/03_RNN/models2.py
@@ -0,0 +1,208 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import hashlib
+import numbers
+
+import tensorflow as tf
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import tensor_util
+from tensorflow.python.layers import base as base_layer
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import clip_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import partitioned_variables
+from tensorflow.python.ops import random_ops
+from tensorflow.python.ops import variable_scope as vs
+from tensorflow.python.ops import variables as tf_variables
+from tensorflow.python.platform import tf_logging as logging
+from tensorflow.python.util import nest
+
+from utils import _linear
+from utils import RNNCell
+
+_BIAS_VARIABLE_NAME = "bias"
+_WEIGHTS_VARIABLE_NAME = "kernel"
+
+class BasicRNNCell(RNNCell):
+  """The most basic RNN cell.
+
+  Args:
+    num_units: int, The number of units in the LSTM cell.
+    activation: Nonlinearity to use.  Default: `tanh`.
+    reuse: (optional) Python boolean describing whether to reuse variables
+     in an existing scope.  If not `True`, and the existing scope already has
+     the given variables, an error is raised.
+  """
+
+  def __init__(self, num_units, activation=None, reuse=None):
+    super(BasicRNNCell, self).__init__(_reuse=reuse)
+    self._num_units = num_units
+    self._activation = activation or math_ops.tanh
+
+  @property
+  def state_size(self):
+    return self._num_units
+
+  @property
+  def output_size(self):
+    return self._num_units
+
+  #self: self class, inputs: first param, state:default null(or 0) 
+  def call(self, inputs, state):
+    ### your code here ###
+    print(self)
+    print(inputs)
+    print(state)
+
+    #_linear:
+    output = self._activation(_linear([inputs, state], self._num_units, True))
+
+    #####################
+	#hydden layer and hydden layer
+    return output, output
+
+
+class GRUCell(RNNCell):
+  """Gated Recurrent Unit cell (cf. http://arxiv.org/abs/1406.1078)."""
+
+  def __init__(self,
+               num_units,
+               activation=None,
+               reuse=None,
+               kernel_initializer=None,
+               bias_initializer=None):
+    super(GRUCell, self).__init__(_reuse=reuse)
+    self._num_units = num_units
+    self._activation = activation or math_ops.tanh
+    self._kernel_initializer = kernel_initializer
+    self._bias_initializer = bias_initializer
+
+  @property
+  def state_size(self):
+    return self._num_units
+
+  @property
+  def output_size(self):
+    return self._num_units
+
+  def call(self, inputs, state):
+    print(self)
+    print(inputs)
+    print(state)
+    """Gated recurrent unit (GRU) with nunits cells."""
+    ### your code here ###
+    with tf.variable_scope("r"):
+        r = tf.sigmoid(_linear([inputs, state], self._num_units, True))
+    with tf.variable_scope("z"):
+        z = tf.sigmoid(_linear([inputs, state], self._num_units, True))
+
+    c = self._activation(_linear([inputs, r * state], self._num_units, True))
+    new_h = (1-z)
+
+    #####################
+    return new_h, new_h
+
+
+_LSTMStateTuple = collections.namedtuple("LSTMStateTuple", ("c", "h"))
+
+
+class LSTMStateTuple(_LSTMStateTuple):
+  """Tuple used by LSTM Cells for `state_size`, `zero_state`, and output state.
+
+  Stores two elements: `(c, h)`, in that order.
+
+  Only used when `state_is_tuple=True`.
+  """
+  __slots__ = ()
+
+  @property
+  def dtype(self):
+    (c, h) = self
+    if c.dtype != h.dtype:
+      raise TypeError("Inconsistent internal state: %s vs %s" %
+                      (str(c.dtype), str(h.dtype)))
+    return c.dtype
+
+
+class BasicLSTMCell(RNNCell):
+  """Basic LSTM recurrent network cell.
+
+  The implementation is based on: http://arxiv.org/abs/1409.2329.
+
+  We add forget_bias (default: 1) to the biases of the forget gate in order to
+  reduce the scale of forgetting in the beginning of the training.
+
+  It does not allow cell clipping, a projection layer, and does not
+  use peep-hole connections: it is the basic baseline.
+
+  For advanced models, please use the full @{tf.nn.rnn_cell.LSTMCell}
+  that follows.
+  """
+
+  def __init__(self, num_units, forget_bias=1.0,
+               state_is_tuple=True, activation=None, reuse=None):
+    """Initialize the basic LSTM cell.
+
+    Args:
+      num_units: int, The number of units in the LSTM cell.
+      forget_bias: float, The bias added to forget gates (see above).
+      state_is_tuple: If True, accepted and returned states are 2-tuples of
+        the `c_state` and `m_state`.  If False, they are concatenated
+        along the column axis.  The latter behavior will soon be deprecated.
+      activation: Activation function of the inner states.  Default: `tanh`.
+      reuse: (optional) Python boolean describing whether to reuse variables
+        in an existing scope.  If not `True`, and the existing scope already has
+        the given variables, an error is raised.
+    """
+    super(BasicLSTMCell, self).__init__(_reuse=reuse)
+    if not state_is_tuple:
+      logging.warn("%s: Using a concatenated state is slower and will soon be "
+                   "deprecated.  Use state_is_tuple=True.", self)
+    self._num_units = num_units
+    self._forget_bias = forget_bias
+    self._state_is_tuple = state_is_tuple
+    self._activation = activation or math_ops.tanh
+
+  @property
+  def state_size(self):
+    return (LSTMStateTuple(self._num_units, self._num_units)
+            if self._state_is_tuple else 2 * self._num_units)
+
+  @property
+  def output_size(self):
+    return self._num_units
+
+  def call(self, inputs, state):
+    print(inputs)
+    print(inputs)
+    print(state)
+    """Long short-term memory cell (LSTM)."""
+	#h:before hydden layer, c:before cell
+    c, h = state
+
+    ### your code here ###
+    with tf.variable_scope("i"):
+        i = tf.sigmoid(_linear([inputs, h], self._num_units, True))
+    with tf.variable_scope("f"):
+        f = tf.sigmoid(_linear([inputs, h], self._num_units, True))
+    with tf.variable_scope("o"):
+        o = tf.sigmoid(_linear([inputs, h], self._num_units, True))
+    with tf.variable_scope("g"):
+        g = tf.sigmoid(_linear([inputs, h], self._num_units, True))
+
+    new_c = f * c + i *g
+    new_h = o * self._activation(new_c)
+
+    ######################
+    new_state = LSTMStateTuple(new_c, new_h)
+    
+    #tensorflow method needs 2 vector: hydden layer and next state(cell)
+    return new_h, new_state
