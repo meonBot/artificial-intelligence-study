3bb031aaec94cc8a7fc05bd3a93c5fc44496735a
jukyellow <45334819+jukyellow@users.noreply.github.com>
2019-05-12 00:44:25 +0900

    Update 02_tensorflow_basic.py

--- 03_Tensorflow_알고리즘구현/02_tensorflow_basic.py
+++ 03_Tensorflow_알고리즘구현/02_tensorflow_basic.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
-### Check TF version
+#1. Check TF version
 tf.__version__
 
+#2. Hello World
 # Create a constant op
 # This op is added as a node to the default graph
 hello = tf.constant("Hello, TensorFlow!")
@@ -13,12 +14,7 @@ sess = tf.Session()
 # run the op and get result
 print(sess.run(hello))
 
-3 # a rank 0 tensor; this is a scalar with shape []
-[1. ,2., 3.] # a rank 1 tensor; this is a vector with shape [3]
-[[1., 2., 3.], [4., 5., 6.]] # a rank 2 tensor; a matrix with shape [2, 3]
-[[[1., 2., 3.]], [[7., 8., 9.]]] # a rank 3 tensor with shape [2, 1, 3]
-
-#Computational Graph
+#3. Computational Graph
 node1 = tf.constant(3.0, tf.float32)
 node2 = tf.constant(4.0) # also tf.float32 implicitly
 node3 = tf.add(node1, node2)
@@ -26,17 +22,26 @@ node3 = tf.add(node1, node2)
 print("node1:", node1, "node2:", node2)
 print("node3: ", node3)
 
-
+#그래프의 값을 출력
 sess = tf.Session()
 print("sess.run(node1, node2): ", sess.run([node1, node2]))
 print("sess.run(node3): ", sess.run(node3))
 
+#4. 노드에 값을 세팅하지 않은채로 진행하는방법->placeholder 노드생성
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
 adder_node = a + b  # + provides a shortcut for tf.add(a, b)
 
+#실행시 placeholder에 값을 전달(feed_dict)하면서 실행
 print(sess.run(adder_node, feed_dict={a: 3, b: 4.5}))
 print(sess.run(adder_node, feed_dict={a: [1,3], b: [2, 4]}))
 
 add_and_triple = adder_node * 3.
 print(sess.run(add_and_triple, feed_dict={a: 3, b:4.5}))
+
+#5. Tensors (=Node)
+#Rank: 차원(1~N차원 array), Shapes: 해당 array의 변수갯수
+3 # a rank 0 tensor; this is a scalar with shape []
+[1. ,2., 3.] # a rank 1 tensor; this is a vector with shape [3]
+[[1., 2., 3.], [4., 5., 6.]] # a rank 2 tensor; a matrix with shape [2, 3]
+[[[1., 2., 3.]], [[7., 8., 9.]]] # a rank 3 tensor with shape [2, 1, 3]
